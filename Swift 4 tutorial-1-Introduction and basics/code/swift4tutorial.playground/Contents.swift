3ae4d07a04bdba3ddd9098ad0d6f3ff16e7f674b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-06-09 14:10:13 +0700

    update

---
+++ Swift 4 tutorial-1-Introduction and basics/code/swift4tutorial.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let pi = 3.1416
+//var x = 102.3
+
+var name = "Hoang", age = 20, email = "sunlight4d@gmail.com"
+print("name = \(name), age = \(age)")
+var x, y, z: Double
+let 🐜🌿 = "I love ant - 🐜"
+let oneBillion = 1_000_000_000; print("one billion = \(oneBillion)")
+
+let aNumber:UInt64 = 100
+let anotherNumber:UInt8 = 10
+let result1 = aNumber + UInt64(anotherNumber)
+print("result 1 = \(result1)")
+
+typealias BigNumber = Double
+let numberOfAnts:BigNumber = 1_000_000
+print("numberOfAnts 1 = \(numberOfAnts)")
+
+var ageNumber = -10;
+precondition(ageNumber >= 0, "Age must be larger than or equal 0")
+var x1 = 100
