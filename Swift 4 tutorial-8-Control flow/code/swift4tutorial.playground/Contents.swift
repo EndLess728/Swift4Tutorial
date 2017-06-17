82798424b7def68ee0f6160baef7da1d7148bdb7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-06-17 14:59:08 +0700

    update

---
+++ Swift 4 tutorial-8-Control flow/code/swift4tutorial.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+//Nguyen Duc Hoang - sunlight4d@gmail.com
+//https://www.youtube.com/c/NguyenDucHoang
+
+import UIKit
+
+for index in 0..<10 {
+//    print("index = \(index)")
+//    print("hello !")
+}
+for index in stride(from: 1, to: 12, by: 0.01) {
+//    print("index =\(index)")
+}
+
+var startValue = 0
+/*
+while startValue < 100 {
+    print("startValue = \(startValue)")
+    startValue = startValue + 1
+}
+ */
+/*
+repeat {
+    print("startValue = \(startValue)")
+    startValue = startValue + 1
+} while startValue < 100
+ */
+
+/*
+var yourPoint = -2
+var estimation:String
+
+switch yourPoint {
+case 0..<2:
+    estimation = "very bad"
+case 2..<5:
+    estimation = "under average"
+case 5..<7:
+    estimation = "average"
+case 7..<9:
+    estimation = "Good"
+case 9...10:
+    estimation = "Exellent"
+default:
+    estimation = "Not a valid value"
+}
+
+print("estimation = \(estimation)")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
