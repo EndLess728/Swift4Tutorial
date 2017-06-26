43b47ea7a40306bbfcd7c38bac1b3bfb154c4dca
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-06-26 17:19:18 +0700

    update

--- Swift 4 tutorial-16-Inheritance/code/swift4tutorial.playground/Contents.swift
+++ Swift 4 tutorial-16-Inheritance/code/swift4tutorial.playground/Contents.swift
@@ -1,47 +1,39 @@
-//Subscript in Swift 4
+//Inheritance in Swift 4
 //Nguyen Duc Hoang - sunlight4d@gmail.com
 //https://www.youtube.com/c/NguyenDucHoang
 
 import UIKit
 
-struct MultiplicationTable {
-    let multiplier: Int
-    subscript(index: Int) -> Int {
-        return multiplier * index
+class Animal {
+    var name: String = ""
+    func eat() {
+        print("Animal is eating...")
+    }
+    var description: String {
+        return "This is an animal named: \(self.name)"
     }
 }
 
-let mt = MultiplicationTable(multiplier: 4)
-/*
-for i in 1...10 {
-    print("\(i) multiplies by 4 is : \(mt[i])")
-}
- */
-
-//mt[2] = 100
-struct AnimalMatrix {
-    var animals = [
-        ["🐜", "🐙", "🐊", "🐎", "🐖"],
-        ["🐢", "🐍", "🦋", "🦀", "🐿"],
-        ["🐌", "🐚", "🐳", "🐒"]
-    ];
-    subscript(row: Int, column: Int) -> String {
-        get {
-            assert(animals[row][column] != nil, "No animal found")
-            return animals[row][column]
-        }
-        set {
-            assert(animals[row][column] != nil, "Cannot set animal value because it is out of range")
-            animals[row][column] = newValue
-        }
+class Cat: Animal {
+    var canClimb: Bool = true
+    func sayMeomeo() {
+        print("I am saying meo meo")
+    }
+    
+    override func eat() {
+        super.eat()
+        print("Cat is eating ...")
+    }
+    
+    override var description: String {
+        return "Cat's name is: \(super.description), can climb = \(self.canClimb)"
     }
 }
+let animal = Animal()
+var myCat = Cat()
+myCat.name = "Mina"
+myCat.eat()
+print(myCat.description)
 
-var am = AnimalMatrix()
-print(am[2, 3])
-am[0, 0] = "xyz"
-print("am = \(am)")
-
-
-
+//myCat.description = "This is meomeo"
 
