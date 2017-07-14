20d27380c60506569fef5aa3f134d2377362d0dc
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-07-15 00:13:15 +0700

    update

--- Swift 4 tutorial-25-Protocols and delegates in Swift/code/swift4tutorial.playground/Contents.swift
+++ Swift 4 tutorial-25-Protocols and delegates in Swift/code/swift4tutorial.playground/Contents.swift
@@ -6,33 +6,62 @@ import UIKit
 //Delegation
 protocol ExampleDelegate {
     func method1()
-    func method2()
 }
+//Delegation enables a class or structure to delegate some of its responsibilities to an instance of another type
+//Class that have delegate property
 class ExampleClass {
     var delegate: ExampleDelegate?
-    func reloadData(){
+    func reloadData() {
         print("running reload data...")
-        print("doing task 1...")
+        print("do some tasks in 1 seconds...")
         delegate?.method1()
-        print("doing task 2...")
-        delegate?.method2()
+        print("do some tasks in 3 seconds...")
+        
     }
 }
 
 class ClassConformDelegate: ExampleDelegate {
     var aProperty: ExampleClass
-    
     init(aProperty: ExampleClass) {
         self.aProperty = aProperty
         self.aProperty.delegate = self
         self.aProperty.reloadData()
     }
+    
+}
+//Adding Protocol Conformance with an Extension
+extension ClassConformDelegate {
     //Implement delegate functions
     func method1() {
         print("ClassConformDelegate calls method1")
     }
-    func method2() {
-        print("ClassConformDelegate calls method2")
+}
+
+let a = ClassConformDelegate(aProperty: ExampleClass())
+
+protocol TextRepresentable {
+    var textualDescription: String {get}
+}
+class Animal:TextRepresentable {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+extension Animal {
+    var textualDescription: String {
+        return "This is an Animal"
     }
 }
-let a = ClassConformDelegate(aProperty: ExampleClass())
+//Optional Protocol Requirements
+@objc protocol ExampleProtocol {
+    @objc optional func method3()
+    @objc optional var property3: Int { get }
+}
+class AnExampleClass: ExampleProtocol {
+    //No need to implement method3 or property3
+}
+
+
+
--- Swift 4 tutorial-26-Generics/code/swift4tutorial.playground/Contents.swift
+++ Swift 4 tutorial-26-Generics/code/swift4tutorial.playground/Contents.swift
@@ -5,38 +5,38 @@
 import UIKit
 //Swift’s Array and Dictionary types are both generic collections
 //Swap 2 integers
-func swapTwoInts(_ a: inout Int, _ b: inout Int) {
-    let temporaryA = a
+/*
+func swap2Ints(_ a: inout Int, _ b: inout Int) {
+    let temp = a
     a = b
-    b = temporaryA
+    b = temp
 }
-var someInt = 3
-var anotherInt = 107
-swapTwoInts(&someInt, &anotherInt)
-print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+var x = 33
+var y = 44
+swap2Ints(&x, &y)
+print("x is now \(x), and y is now \(y)")
+ */
 //T = placeholder type name
-func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
-    let temporaryA = a
+func swap2Values<T>(_ a: inout T, _ b: inout T) {
+    let temp = a
     a = b
-    b = temporaryA
+    b = temp
 }
-var string1 = "hello"
-var string2 = "world"
-swapTwoValues(&string1, &string2)
+var string1 = "str1"
+var string2 = "str2"
+swap2Values(&string1, &string2)
 print("string1 is now \(string1), and string2 is now \(string2)")
-//Define Stack struct with type "Element"
-struct Stack<Element> {
-    var items = [Element]()
-     //mutating => because they need to modify/mutate the structure’s items array
-    mutating func push(_ item: Element) {
+//Define Stack struct with type "Element" or "T"
+struct Stack<T> {
+    var items = [T]()
+    //mutating => because they need to modify/mutate the structure’s items array
+    mutating func push(_ item: T) {
         items.append(item)
     }
-    mutating func pop() -> Element? {
+    mutating func pop() -> T? {
         return items.count == 0 ? nil : items.removeLast()
-//        return items.removeLast()
     }
 }
-
 var integerStack = Stack<Int>()
 integerStack.push(1)
 integerStack.push(2)
@@ -55,8 +55,41 @@ func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
     }
     return nil
 }
-
-let intIndex = findIndex(of: 10, in: [10, 11, 12])
+let intIndex = findIndex(of: 11, in: [10, 11, 12])
 let stringIndex = findIndex(of: "Hoang", in: ["Alex", "Peter", "Mary"])
+//Associated Types
+protocol Container {
+    associatedtype T
+    mutating func append(_ item: T)
+    var count: Int { get }
+}
+
+struct IntStack: Container {
+    // original IntStack implementation
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    typealias T = Int
+    mutating func append(_ item: T) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    
+}
+
+
+
+
+
+
+
+
 
 
